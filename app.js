d07709305fa68f85724b10d5a92591b2e7b2ce63
sumin2710 <su91582710@gmail.com>
2024-02-07 23:01:31 +0900

    FEAT: 팔로우 구현

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import UsersRouter from './routers/users.router.js';
 import AdminRouter from './routers/admin.router.js';
+import FollowsRouter from './routers/follows.router.js';
 import ResumesRouter from './routers/resumes.router.js';
 import errorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 import cookieParser from 'cookie-parser';
@@ -26,6 +27,7 @@ app.use(cookieParser());
 
 app.use('/api', [UsersRouter, ResumesRouter]);
 app.use('/admin', [AdminRouter]);
+app.use('/follow', [FollowsRouter]);
 app.use(errorHandlingMiddleware);
 
 app.listen(PORT, () => {
